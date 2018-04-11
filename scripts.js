abb33725a70f703615e95379fd295ce3b0600d0d
Adam Kollberg <adamkollberg@Adams-Air.lan>
2018-04-11 11:46:57 +0200

    this ones done

--- scripts.js
+++ scripts.js
@@ -25,7 +25,10 @@ function paintToCanvas (){
     return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
     let pixels = ctx.getImageData(0,0, width, height);
-    pixels = redEffect(pixels);
+    //pixels = redEffect(pixels);
+
+    //pixels = rgbSplit(pixels);
+pixels = greenScreen (pixels);
     ctx.putImageData(pixels,0,0);
     }, 16);
 }
@@ -49,6 +52,40 @@ function redEffect(pixels){
 
     }
 return pixels;
+}
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4){
+        pixels.data [i-150] = pixels.data [i+0]; //red
+        pixels.data [i+100] = pixels.data [i+1]  //green
+        pixels.data [i-150] = pixels.data [i+2];
+
+    }
+return pixels;
+
+}
+function greenScreen(pixels){
+    const levels = {};
+
+    [...document.querySelectorAll('.rgb input')].forEach((input) =>{
+        levels[input.name] = input.value;
+
+    });
+    
+    for (i = 0; i < pixels.data.length; i = i +4) {
+        red = pixels.data[i +0];
+        green = pixels.data[i +1];
+        blue = pixels.data[i +2];
+        alpha = pixels.data[i +3];
+
+        if (red >= levels.rmin
+         && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+         && green <= levels.gmax
+        && blue <= levels.bmax) {
+            pixels.data[i + 3] = 0;
+        }
+    }
 }
 
 getVideo();
