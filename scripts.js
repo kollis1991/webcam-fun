e4412df7ba305b3fbbd27bc76e31d408cec69b61
Adam Kollberg <adamkollberg@Adams-Air.lan>
2018-04-11 11:27:39 +0200

    making the oicture red

--- scripts.js
+++ scripts.js
@@ -24,6 +24,9 @@ function paintToCanvas (){
 
     return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0,0, width, height);
+    pixels = redEffect(pixels);
+    ctx.putImageData(pixels,0,0);
     }, 16);
 }
 function takePhoto(){
@@ -38,5 +41,15 @@ function takePhoto(){
     strip.insertBefore(link, strip.firstChild);
 
 }
+function redEffect(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4){
+        pixels.data [i+0] = pixels.data [i+0] + 100; //red
+        pixels.data [i+1] = pixels.data [i+1] - 50; //green
+        pixels.data [i+2] = pixels.data [i+2] * 0.5;
+
+    }
+return pixels;
+}
+
 getVideo();
 video.addEventListener('canplay', paintToCanvas);
